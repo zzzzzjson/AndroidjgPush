5e8a13f695d88a3a007dca6edab5898d08081196
zzzzzjson <44796239+zzzzzjson@users.noreply.github.com>
2018-12-13 20:48:36 +0800

    提交

---
+++ androidjpushproject/src/main/java/soexample/umeng/com/androidjpushproject/MainActivity.java
@@ -0,0 +1,13 @@
+package soexample.umeng.com.androidjpushproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
